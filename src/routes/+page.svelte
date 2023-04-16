ec3c981755ef8b52927a6a158970c37df200a57f
Michal Hicz <michal07.h@gmail.com>
2023-04-17 01:06:52 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+	import Counter from './Counter.svelte';
+	import logo from '$lib/images/airisp.png';
+</script>
+
+<svelte:head>
+	<title>airISP</title>
+	<meta name="description" content="airISP" />
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<img src={logo} alt="" />
+			</picture>
+		</div>
+	</h1>
+	<p class="drac-heading drac-heading-lg drac-text-white">Odstávka služieb na:</p>	
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	@media only screen and (max-width: 450px) {
+		h1 {
+			font-size: 1.8rem;
+		}
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		padding: 0 0 0 0;
+		align-items: center;
+	}
+
+	.welcome img {
+		height: 8em;
+		margin: auto;
+		display: block;
+		pointer-events: none;
+	
+	}
+</style>
---
+++ src/routes/podmienky/+page.svelte
@@ -0,0 +1,58 @@
+<svelte:head>
+	<title>Podmienky služby</title>
+	<meta name="description" content="Podmienky" />
+</svelte:head>
+
+
+
+<div class="text-column">
+	<div class="drac-box drac-card drac-card-subtle drac-border-purple drac-p-md drac-m-md">
+    <span class="drac-text drac-line-height drac-text-white">
+		<b>Vitajte v airISP! Používaním našich internetových služieb súhlasíte s tým, že budete viazaní nasledujúcimi zmluvnými podmienkami (TOS). Ak nesúhlasíte s týmito podmienkami, nepoužívajte naše služby.<br><br>
+</b>
+<b>Dostupnosť a používanie služby:<br><br></b>
+1.1 airISP poskytuje svojim zákazníkom prístup na internet v súlade s týmito VOP. Môžeme upraviť, aktualizovať alebo ukončiť naše služby, úplne alebo čiastočne, kedykoľvek bez predchádzajúceho upozornenia.
+<br><br>
+1.2 Zodpovedáte za to, že akékoľvek používanie našich služieb je v súlade s týmito VOP, ako aj s príslušnými zákonmi a predpismi.
+<br><br>
+1.3 Ste výhradne zodpovední za všetky aktivity, ktoré sa vyskytnú v rámci vášho účtu, vrátane akéhokoľvek neoprávneného použitia vášho účtu alebo akéhokoľvek iného narušenia bezpečnosti.
+<br><br>
+<b>Servisné poplatky a fakturácia:<br><br></b>
+2.1 Naše poplatky za službu sú uvedené na našej webovej stránke alebo inak písomne dohodnuté stranami.
+<br><br>
+2.2 Naše poplatky môžeme z času na čas upraviť zverejnením nových poplatkov na našej webovej stránke alebo písomným oznámením. Vaše ďalšie používanie našich služieb po takejto úprave poplatkov predstavuje váš súhlas s novými poplatkami.
+<br><br>
+2.3 Platba za služby je splatná v deň, ktorý je uvedený na vašej faktúre alebo ako sa zmluvné strany písomne dohodnú inak.
+<br><br>
+2.4 Ak nezaplatíte akékoľvek sumy splatné za naše služby, môžeme pozastaviť alebo ukončiť váš prístup k našim službám bez predchádzajúceho upozornenia.
+<br><br>
+<b>Zakázané použitie:<br><br></b>
+3.1 Súhlasíte s tým, že nebudete používať naše služby na žiadne nezákonné, podvodné alebo neoprávnené účely.
+<br><br>
+3.2 Súhlasíte s tým, že nebudete používať naše služby na obťažovanie, zneužívanie, vyhrážanie sa alebo iné porušovanie zákonných práv iných.
+<br><br>
+3.3 Súhlasíte s tým, že nebudete používať naše služby na prenos akýchkoľvek vírusov, červov, malvéru alebo iného škodlivého alebo deštruktívneho kódu.
+<br><br>
+<b>Duševné vlastníctvo:<br><br></b>
+4.1 Všetok obsah na našej webovej stránke vrátane textu, grafiky, log, obrázkov a softvéru je vlastníctvom airISP alebo jej poskytovateľov licencií a je chránený autorským právom a inými zákonmi o duševnom vlastníctve.
+<br><br>
+4.2 Súhlasíte s tým, že nebudete kopírovať, distribuovať ani inak používať akýkoľvek náš obsah bez nášho predchádzajúceho písomného súhlasu.
+<br><br>
+<b>Obmedzenie zodpovednosti:<br><br></b>
+5.1 airISP nezodpovedá za žiadne škody vyplývajúce z používania alebo neschopnosti využívať naše služby, vrátane, ale nie výlučne, nepriamych, náhodných, špeciálnych alebo následných škôd.
+<br><br>
+5.2 Zodpovednosť airISP voči vám v žiadnom prípade nepresiahne celkovú sumu, ktorú ste zaplatili za naše služby počas obdobia na faktúre pred podaním nároku.
+<br><br>
+<b>Odškodnenie:<br><br></b>
+6.1 Súhlasíte s tým, že odškodníte a ochránite spoločnosť airISP a jej pridružené spoločnosti, úradníkov, riaditeľov, zamestnancov a zástupcov pred akýmikoľvek nárokmi, požiadavkami alebo škodami vyplývajúcimi z vášho používania našich služieb alebo z vášho porušenia týchto TOS.
+<br><br>
+<b>Ukončenie:<br><br></b>
+7.1 Tieto VOP zostanú v plnej platnosti a účinnosti počas používania našich služieb.
+<br><br>
+7.2 Môžeme ukončiť tieto TOS alebo pozastaviť váš prístup k našim službám kedykoľvek a z akéhokoľvek dôvodu, bez predchádzajúceho upozornenia.
+<br><br>
+7.3 Po ukončení musíte okamžite prestať používať naše služby.
+	</span>
+  </div>
+</div>
+
