39bafa4f3db1d1a6c2904ca28997d95a2461f9b4
Michal Hicz <michal07.h@gmail.com>
2023-11-14 14:03:42 +0100

    update to svelte 4

--- src/routes/podmienky/+page.svelte
+++ src/routes/podmienky/+page.svelte
@@ -13,7 +13,7 @@
 		<b>Všetok náš obsah je voľne šíritelný a dostupný pre každého bez akýchkoľvek poplatkov. Obsah nebude nikdy spoplatnený a slúži len na informačné účely.</b>
 <br><br><b>Zdrojový kód:<br><br></b>
 Zdrojový kód tejto webovej stránky spolu s celým obsahom je voľne dostupný na githube (odkaz v pravom hornom rohu) pod licenciou GNU GENERAL PUBLIC LICENSE Version 3.</span>
-<br><br><a class="drac-text" href="https://github.com/Sietove-vzdelanie/sietovevzdelanie-web/blob/main/LICENSE">ODKAZ NA LICENCIU</a>
+<br><br><a style="color: #9580FF;" class="drac-text" href="https://github.com/Sietove-vzdelanie/sietovevzdelanie-web/blob/main/LICENSE">ODKAZ NA LICENCIU</a>
   </div>
   <center><a href="/" style="margin-top: 1em; margin-left:1em; width:6em" class="drac-btn drac-bg-purple drac-m-sm drac-btn-lg">
 	SPÄŤ</a></center>
